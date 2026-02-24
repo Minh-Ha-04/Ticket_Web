722ea2eacfd17b56731d9c9e58f9cf0a0cc733b1
Quang Minh <quangminhh2004@gmail.com>
2026-02-24 18:34:34 +0700

    .

--- be/src/app.js
+++ be/src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import router from "./routes/index.js";
 import cors from "cors";
 import passport from "./config/passport.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,8 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running 🚀");
+});
+
 app.use("/api", router);
-
-
 
 export default app;
