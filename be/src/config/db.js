3799d7fddb9711655e728f65449d3b66866ec6f4
Quang Minh <quangminhh2004@gmail.com>
2026-02-24 17:29:31 +0700

    update port

--- be/src/config/db.js
+++ be/src/config/db.js
@@ -1,17 +1,23 @@
-import {Sequelize} from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: "mysql",
     timezone: "+07:00",
     dialectOptions: {
-    dateStrings: true, 
-    typeCast: true,
+      dateStrings: true,
+      typeCast: true,
     },
     logging: false,
-});
+  }
+);
 
 export default sequelize;
